b225398b9cbdcaf656651fb9c79d4b882ab9d17b
TIny_Hacker <thetiniesthacker@gmail.com>
2025-11-30 21:23:33 -0500

    Initial commit

---
+++ convertSampleInput.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ ! -f "input.txt" ]] # Ensure the file was downloaded
+then
+    echo "No input file found."
+    exit 1
+fi
+
+echo "Converting newlines to null..."
+
+tr '\n' '\0' < input.txt > input # Zero terminate newlines
+
+mv input input.txt
+
+echo "Converting output to 8xv..."
+
+convbin -j bin -k 8xv -i input.txt -o SampleInput.8xv -n Input # Convert to appvar
+
+echo "Restoring input..."
+
+tr '\0' '\n' < input.txt > input # Restore readable input
+mv input input.txt
+
+if [[ ! -f "SampleInput.8xv" ]] # Ensure the files were created
+then
+    echo "Something went wrong."
+    exit 1
+fi
+
+echo "Success!"
+exit 0
